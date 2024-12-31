934e396d63c324261365b78bb124429205644e1b
artinfbi <74910626+artinfbi@users.noreply.github.com>
2024-12-31 04:05:14 +0330

    Add files via upload

---
+++ WARP_FIXER.ps1
@@ -0,0 +1,13 @@
+﻿
+while ($true) {
+    try {
+        Disable-NetAdapterBinding -Name "CloudflareWARP" -ComponentID ms_tcpip6
+	Write-Progress -Activity "Working fine" 
+	
+    } catch {
+        Write-Host "Error occurred: $_" -ForegroundColor Red
+	Write-Progress -Activity "Eror"
+    }
+    Start-Sleep -Seconds 0.1
+    
+}
